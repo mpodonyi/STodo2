92d0b6de36351bd3f4cc28fcedff2f03ad66dc83
Mike Podonyi <mikepodonyi@gmail.com>
2016-07-06 09:18:00 +0100

    Initial Commit

---
+++ STodoLib.Test/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace MyFirstDotNetCoreTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var tt = new STodoLib.Class1();
+
+            tt.Fire();
+
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ STodoLib/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STodoLib
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public void Fire()
+        {
+
+        }
+
+    }
+
+
+    public static class TodoLoader
+    {
+        public static TodoObject GetObject(string path)
+        {
+            return new TodoFileObject(System.IO.File.ReadAllText(path));
+        }
+
+    }
+
+
+
+
+
+
+
+    public abstract class TodoObject
+    {
+
+
+
+    }
+
+    public class TodoFileObject : TodoObject
+    {
+        public TodoFileObject(string content)
+        {
+
+        }
+    }
+
+
+
+
+    public class TodoSection
+    {
+
+
+    }
+
+
+    public class TodoItem
+    {
+    }
+
+
+}
